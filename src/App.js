182a9c49e840bcf483d08fdff392641726e8d42d
Sebastian Glaser <hakt0r@users.noreply.github.com>
2021-02-14 00:20:44 +0100

    confetti2

--- src/App.js
+++ src/App.js
@@ -9,22 +9,23 @@ import Show  from './Show';
 import Words from './Words';
 import Confetti from 'react-dom-confetti';
 
+const config = {
+  angle: 90,
+  spread: 360,
+  startVelocity: 40,
+  elementCount: 200,
+  dragFriction: 0.12,
+  duration: 3000,
+  stagger: 3,
+  width: "10px",
+  height: "10px",
+  perspective: "500px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
+
 function App() {
   const dispatch = useDispatch();
   const youWin = useSelector( ({youWin}) => youWin );
-  const config = {
-    angle: 90,
-    spread: 360,
-    startVelocity: 40,
-    elementCount: 200,
-    dragFriction: 0.12,
-    duration: 3000,
-    stagger: 3,
-    width: "10px",
-    height: "10px",
-    perspective: "500px",
-    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
-  };
   return <div className={ youWin ? 'win' : null }>
   <div className="menuButton">
     <IconButton color="inherit" onClick={ e => dispatch({type:'editorToggle'})}>
