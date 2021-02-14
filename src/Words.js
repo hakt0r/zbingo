4a7b9fdffcb101643b472ea589e93990a4e4a08b
Sebastian Glaser <hakt0r@users.noreply.github.com>
2021-02-14 10:57:10 +0100

    wtf1

--- src/Words.js
+++ src/Words.js
@@ -3,10 +3,25 @@ import { TextField } from '@material-ui/core'
 import { Button } from '@material-ui/core'
 import { Cancel, Save } from '@material-ui/icons'
 import React from 'react'
+import { useRef } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 const Words = () => {
   const dispatch = useDispatch();
+  const ref = useRef(null);
+  useEffect( () => {
+    if ( ! ref.current ) return;
+    var observer = new MutationObserver( function( mutations ){
+      mutations.forEach( function( mutation ){
+        if ( mutation.attributeName === 'style' && window.getComputedStyle( ref.current ).getPropertyValue( 'height' ) !== '100%' ){
+          ref.current.style.height = "100%";
+          ref.current.style.overflow = 'unset';
+        }
+      });
+    });
+    observer.observe( ref.current, { attributes: true } );
+  }, [ref] );
   const { wordsInEditor, titleInEditor } = useSelector( ({wordsInEditor,titleInEditor}) => ({ wordsInEditor, titleInEditor }) );
   return (
     <Paper className="dialog">
@@ -18,7 +33,7 @@ const Words = () => {
         <Button color="primary" variant="contained" onClick={ e => dispatch({type:'editorSave'}) }><Save/></Button>
       </div>
       <div className="bottom">
-        <TextField fullWidth multiline variant="outlined"
+        <TextField fullWidth multiline variant="outlined" inputRef={ref}
           value={wordsInEditor} onChange={ e => dispatch({type:'editorChange',value:e.target.value})}
         />
       </div>
